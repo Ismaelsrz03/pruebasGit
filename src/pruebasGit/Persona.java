1cb47ed98d5d0313304f0e00862a033ef87d9271
Ismaelsrz2003 <ismaelsrz03@gmail.com>
2022-10-25 09:42:45 +0200

    Tercer commit personas getters y setters

--- src/pruebasGit/Persona.java
+++ src/pruebasGit/Persona.java
@@ -4,7 +4,26 @@ public class Persona {
 
 	private String nombre;
 	private int edad;
-	private double peso; 
+	private double peso;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	} 
+	
 	
 	
 }
